97c52f7a0ad0c9491b562fabaab4e915bab2384e
tnhkid <47786359+tnhkid@users.noreply.github.com>
2021-04-24 13:13:28 -0400

    Add files via upload

--- src/main/java/com/example/springtemplate/daos/UserJdbcDao.java
+++ src/main/java/com/example/springtemplate/daos/UserJdbcDao.java
@@ -8,16 +8,16 @@ import java.util.*;
 public class UserJdbcDao {
     static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String HOST = "localhost:3306";
-    static final String SCHEMA = "YOUR_SCHEMA";
+    static final String SCHEMA = "db_journals";
     static final String CONFIG = "serverTimezone=UTC";
     static final String URL =
             "jdbc:mysql://"+HOST+"/"+SCHEMA+"?"+CONFIG;
     static final String USERNAME = "YOUR_USERNAME";
-    static final String PASSWORD = "YOUR_PASSWORD";
+    static final String PASSWORD = "P@ssw0rd";
     
     static Connection connection = null;
     static PreparedStatement statement = null;
-    String CREATE_USER = "INSERT INTO users VALUES (null, ?, ?, ?, ?, ?, null, null)";
+    String CREATE_USER = "INSERT INTO users VALUES (null, ?, ?, ?, ?, ?, ?)";
     String FIND_ALL_USERS = "SELECT * FROM users";
     String FIND_USER_BY_ID = "SELECT * FROM users WHERE id=?";
     String DELETE_USER = "DELETE FROM users WHERE id=?";
@@ -45,7 +45,8 @@ public class UserJdbcDao {
                     resultSet.getString("password"),
                     resultSet.getString("first_name"),
                     resultSet.getString("last_name"),
-                    resultSet.getString("profile_picture")
+                    resultSet.getString("email"),
+                    resultSet.getString("date_of_birth")
             );
         }
         closeConnection(connection);
@@ -87,7 +88,8 @@ public class UserJdbcDao {
                     resultSet.getString("password"),
                     resultSet.getString("first_name"),
                     resultSet.getString("last_name"),
-                    resultSet.getString("profile_picture")
+                    resultSet.getString("email"),
+                    resultSet.getString("date_of_birth")
             );
             users.add(user);
         }
@@ -103,7 +105,8 @@ public class UserJdbcDao {
         statement.setString(2, user.getPassword());
         statement.setString(3, user.getFirstName());
         statement.setString(4, user.getLastName());
-        statement.setString(5, user.getProfilePicture());
+        statement.setString(5, user.getEmail());
+        statement.setString(6, user.getDateOfBirth());
         rowsUpdated = statement.executeUpdate();
         closeConnection(connection);
         return rowsUpdated;
@@ -111,8 +114,8 @@ public class UserJdbcDao {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("JDBC DAO");
         UserJdbcDao dao = new UserJdbcDao();
-//        User adam = new User("Adam", "Smith", "adams", "invisiblehand", "http://bbc.in/30gXhI4");
-//        User catherine = new User("Catherine", "Wood", "cathie", "bitcoinisbig", "https://ark-invest.com/");
+//        User adam = new User("Adam", "Smith", "adams", "invisiblehand", "asmith@gmail.com", "06-05-1723");
+//        User catherine = new User("Catherine", "Wood", "cathie", "bitcoinisbig", "cwood@gmail.com", "11-26-1955");
 //        dao.createUser(adam);
 //        dao.createUser(thomas);
 //        dao.createUser(catherine);
@@ -127,15 +130,10 @@ public class UserJdbcDao {
 //        for(User user: users) {
 //            System.out.println(user.getUsername());
 //        }
-        User thomas = new User("Thomas", "Sowell", "thomas", "polymath", "http://www.tsowell.com/");
-        User newTom = new User(
-                "Tom",
-                "Sowell",
-                "tom",
-                "knowitall",
-                thomas.getProfilePicture());
-        dao.updateUser(6, newTom);
-        User tom = dao.findUserById(6);
-        System.out.println(tom.getUsername());
+//        User thomas = new User("Thomas", "Sowell", "thomas", "polymath", "tsowell@gmail.com", "02-14-1990");
+//        User newTom = new User( "Tom", "Sowell", "tom", "knowitall", thomas.getEmail(), thomas.getDateOfBirth());
+//        dao.updateUser(6, newTom);
+//        User tom = dao.findUserById(6);
+//        System.out.println(tom.getUsername());
     }
 }
